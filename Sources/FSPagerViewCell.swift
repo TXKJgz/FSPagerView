d68e5a6679b8c3450b91913f49875c4605f9cab8
helinyu <2319979647@qq.com>
2025-06-30 12:20:24 +0800

    去掉阴影

--- Sources/FSPagerViewCell.swift
+++ Sources/FSPagerViewCell.swift
@@ -101,10 +101,10 @@ open class FSPagerViewCell: UICollectionViewCell {
     fileprivate func commonInit() {
         self.contentView.backgroundColor = UIColor.clear
         self.backgroundColor = UIColor.clear
-        self.contentView.layer.shadowColor = UIColor.black.cgColor
-        self.contentView.layer.shadowRadius = 5
-        self.contentView.layer.shadowOpacity = 0.75
-        self.contentView.layer.shadowOffset = .zero
+//        self.contentView.layer.shadowColor = UIColor.black.cgColor
+//        self.contentView.layer.shadowRadius = 5
+//        self.contentView.layer.shadowOpacity = 0.75
+//        self.contentView.layer.shadowOffset = .zero
     }
     
     deinit {
@@ -116,7 +116,7 @@ open class FSPagerViewCell: UICollectionViewCell {
     override open func layoutSubviews() {
         super.layoutSubviews()
         
-        self.contentView.layer.shadowPath = UIBezierPath(rect: self.contentView.bounds).cgPath
+//        self.contentView.layer.shadowPath = UIBezierPath(rect: self.contentView.bounds).cgPath
 
         if let imageView = _imageView {
             imageView.frame = self.contentView.bounds
